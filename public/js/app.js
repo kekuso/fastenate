8367f0a6f7c6faa895d078736bcc352814d146c5
kekuso <kekuso808@gmail.com>
2016-06-18 20:54:59 -1000

    Cleaned up the header.

--- public/js/app.js
+++ public/js/app.js
@@ -32,40 +32,40 @@ function display(response) {
     var threads = document.createElement("div");
 
   //mainPage
-  mainPage.backgroundColor = '#fff';
+  //mainPage.backgroundColor = '#fff';
 
   //header
-  header.setAttribute("id", "header");
+  header.setAttribute("class", "header");
   //header.backgroundColor = "#fff";
   mainPage.appendChild(header);
 
   //headerBackground
-  headerBackground.setAttribute("id", "headerBackground");
-  headerBackground.src = "/assets/header_bg.svg";
-  header.appendChild(headerBackground);
+  // headerBackground.setAttribute("id", "headerBackground");
+  // headerBackground.src = "/assets/header_bg.svg";
+  //header.appendChild(headerBackground);
   //logo
-  logo.setAttribute("id", "logo");
-  logo.src = "/assets/logo.svg";
-  header.appendChild(logo);
+  // logo.setAttribute("id", "logo");
+  // logo.src = "/assets/logo.svg";
+  //header.appendChild(logo);
 
   //menu
-  menu.setAttribute("id", "menu");
+  menu.setAttribute("class", "menu");
   mainPage.appendChild(menu);
 
   //random
-  random.setAttribute("id", "random");
+  random.setAttribute("class", "random");
   random.className = "menuItems";
   random.innerHTML = "RANDOM";
   menu.appendChild(random);
 
   //myBoards
-  myBoards.setAttribute("id", "myBoards");
+  myBoards.setAttribute("class", "myBoards");
   myBoards.className = "menuItems";
   myBoards.innerHTML = "MY BOARDS";
   menu.appendChild(myBoards);
 
   //getTheApp
-  getTheApp.setAttribute("id", "getTheApp");
+  getTheApp.setAttribute("class", "getTheApp");
   getTheApp.className = "menuItems";
   getTheApp.innerHTML = "GET THE APP";
   menu.appendChild(getTheApp);
@@ -82,15 +82,48 @@ function display(response) {
     title = document.createElement("h1");
     imageContainer = document.createElement("div");
     author = document.createElement("h3");
+    createDate = document.createElement("div");
 
-
+    author.innerHTML = "author: " + data.author;
+    createDate.innerHTML = "created: " + getDate(data.created);
     threadImage.src = data.url;
     title.innerHTML = data.title;
+
     imageContainer.appendChild(threadImage);
-    threads.appendChild(imageContainer);
+    //threads.appendChild(imageContainer);
     threads.appendChild(title);
+    threads.appendChild(author);
+    threads.appendChild(createDate);
   }
 
   mainPage.appendChild(threads);
   // data = response.data.children[1].data.domain;
+}
+
+function getDate(timestamp) {
+  var date = new Date(timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = date.getFullYear();
+  var month = months[date.getMonth()];
+  var day = date.getDate();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var time;
+
+  if(min < 10) {
+    min = '0' + min.toString();
+  }
+  if(hour > 12) {
+    if(hour < 13) {
+      hour = 12;
+    }
+    else {
+      hour = hour - 12;
+    }
+    time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + 'PM (HST)';
+  }
+  else {
+    time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + 'AM (HST)';
+  }
+  return time;
 }
